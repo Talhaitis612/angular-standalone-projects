0bdc99b4887066f18c0884748381d10234120556
Talhaitis612 <bsem-f18-206@superior.edu.pk>
2023-02-04 13:50:44 +0500

    stand alone components in working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { DetailsComponent } from './welcome/details/details.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
+  standalone : true,
+  imports: [WelcomeComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
